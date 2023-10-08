9111bb3aa9c24615c0bb86c297ed369aa48dae1e
shaynedsouza <35337020+shaynedsouza@users.noreply.github.com>
2023-10-09 01:49:16 +0530

    tablet toggle

--- Assets/Scripts/Managers/TabletManager.cs
+++ Assets/Scripts/Managers/TabletManager.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 public class TabletManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Used to show/hids tablet
+    bool isTabletEnabled = false;
+
+    public InputActionAsset inputActionReference;
+    public string controllerName;
+    public string actionToggleTablet;
+    InputActionMap inputActionMap;
+    InputAction inputActionPrimaryButton;
+    public float value;
+
+
+    private void Awake()
     {
-        
+        transform.localScale = Vector3.zero;
+        isTabletEnabled = false;
+        inputActionMap = inputActionReference.FindActionMap(controllerName);
+        inputActionPrimaryButton = inputActionMap.FindAction(actionToggleTablet);
+        inputActionPrimaryButton.performed += ToggleTablet;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        inputActionPrimaryButton.Enable();
     }
+
+    private void OnDisble()
+    {
+        inputActionPrimaryButton.Disable();
+    }
+
+
+
+    public void ToggleTablet(CallbackContext context)
+    {
+        if (isTabletEnabled)
+        {
+            transform.localScale = Vector3.zero;
+            isTabletEnabled = false;
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            isTabletEnabled = true;
+        }
+    }
+
+    // private void Update()
+    // {
+    //     var primaryButtonValue = inputActionPrimaryButton.ReadValue<float>();
+    //     value = inputActionPrimaryButton.ReadValue<float>();
+    // }
+
 }
