cbd0bea979b276d27ac1e5af0122254fd6414976
shaynedsouza <35337020+shaynedsouza@users.noreply.github.com>
2023-10-22 12:16:50 +0530

    finger anim working

--- Assets/Scripts/Player/FingerAnimation.cs
+++ Assets/Scripts/Player/FingerAnimation.cs
@@ -10,13 +10,18 @@ public class FingerAnimation : MonoBehaviour
     [SerializeField] Animator animator;
     InputActionMap inputActionMapLeft, inputActionMapRight;
     InputAction inputActionTriggerLeft, inputActionTriggerRight, inputActionGripLeft, inputActionGripRight;
+    InputAction inputActionA, inputActionB, inputActionX, inputActionY;
     [SerializeField] string controllerNameLeft, controllerNameRight;
     [SerializeField] string triggerButton;
     [SerializeField] string gripButton;
+    [SerializeField] string aButton;
+    [SerializeField] string bButton;
+    [SerializeField] string xButton;
+    [SerializeField] string yButton;
     [SerializeField] PhotonView photonView;
+    //grip, trigger, b/y, a/x
 
-
-
+    public float a, b, x, y;
 
     private void Awake()
     {
@@ -31,6 +36,13 @@ public class FingerAnimation : MonoBehaviour
         inputActionGripLeft = inputActionMapLeft.FindAction(gripButton);
         inputActionGripRight = inputActionMapRight.FindAction(gripButton);
 
+
+        inputActionA = inputActionMapRight.FindAction(aButton);
+        inputActionB = inputActionMapRight.FindAction(bButton);
+
+        inputActionX = inputActionMapLeft.FindAction(xButton);
+        inputActionY = inputActionMapLeft.FindAction(yButton);
+
         // Debug.Log(inputActionMapLeft.name);
         // Debug.Log(inputActionMapRight.name);
 
@@ -43,6 +55,10 @@ public class FingerAnimation : MonoBehaviour
         inputActionTriggerRight.Enable();
         inputActionGripLeft.Enable();
         inputActionGripRight.Enable();
+        inputActionA.Enable();
+        inputActionB.Enable();
+        inputActionX.Enable();
+        inputActionY.Enable();
     }
 
     private void OnDisble()
@@ -51,6 +67,10 @@ public class FingerAnimation : MonoBehaviour
         inputActionTriggerRight.Disable();
         inputActionGripLeft.Disable();
         inputActionGripRight.Disable();
+        inputActionA.Disable();
+        inputActionB.Disable();
+        inputActionX.Disable();
+        inputActionY.Disable();
     }
 
 
@@ -60,9 +80,23 @@ public class FingerAnimation : MonoBehaviour
         if (!photonView.IsMine) return;
 
         animator.SetFloat("l_thumb", inputActionTriggerLeft.ReadValue<float>());
-        animator.SetFloat("l_fingers", inputActionGripLeft.ReadValue<float>());
         animator.SetFloat("r_thumb", inputActionTriggerRight.ReadValue<float>());
-        animator.SetFloat("r_fingers", inputActionGripRight.ReadValue<float>());
+
+        animator.SetFloat("l_index", inputActionGripLeft.ReadValue<float>());
+        animator.SetFloat("r_index", inputActionGripRight.ReadValue<float>());
+
+
+        animator.SetFloat("l_middle", inputActionY.ReadValue<float>());
+        animator.SetFloat("r_middle", inputActionB.ReadValue<float>());
+
+        animator.SetFloat("l_ring", inputActionX.ReadValue<float>());
+        animator.SetFloat("r_ring", inputActionA.ReadValue<float>());
+
+
+        a = inputActionA.ReadValue<float>();
+        x = inputActionX.ReadValue<float>();
+        b = inputActionB.ReadValue<float>();
+        y = inputActionY.ReadValue<float>();
     }
 
 
